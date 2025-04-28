97d31f81992035825f985381323fe3d8f1cc45e3
Abu Talha Tamim <talha@boishakhikunja.onmicrosoft.com>
2025-04-28 13:10:55 +0600

    01-Fundamentals-Part-1

---
+++ assignments.js
@@ -0,0 +1,18 @@
+// Variables and Values
+const country = "Bangladesh";
+const continent = "Asia";
+const population = 171500000;
+
+// Data Types
+const isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+// let, const and var
+language = "Bangla";
+// continent = "Europe";
+
+// Basic Operators
---
+++ script.js
@@ -0,0 +1,57 @@
+/*
+// Variables and Values
+let js = "amazing";
+if (js === "amazing") alert("Javascript is FUN!");
+
+console.log(40 + 8 + 23 - 10);
+console.log(23);
+
+let firstName = "Jesus";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+*/
+
+/*
+// Data Types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jesus Christ");
+
+javascriptIsFun = "Yeas!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+console.log(typeof null);
+*/
+
+/*
+// let, const and var
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = "programmer";
+job = "teacher";
+console.log(job);
+
+lastName = "Christ";
+console.log(lastName);
+*/
+
+// Basic Operators
+const ageJonas = 2037 - 1991;
+const ageSarah = 2037 - 2018;
+console.log(ageJonas, ageSarah);
